aa0c326b4e3659d055c4436115f67dee7cb06abd
unknown <davidyoungtimes@gmail.com>
2023-05-13 13:07:19 +0200

    test

---
+++ src/app/services/test.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@angular/core';
+import { Camera, CameraResultType, CameraSource, Photo } from '@capacitor/camera';
+import { Filesystem, Directory } from '@capacitor/filesystem';
+import { Preferences } from '@capacitor/preferences';
+import { Platform } from '@ionic/angular';
+import { Capacitor } from '@capacitor/core';
+
+export class PhotoService {
+  
+ public test = "test"
+    }
